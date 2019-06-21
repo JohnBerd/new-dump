86bff5aa659a9412b620375fff44bf17bd72d4ed
root <root@localhost.localdomain>
2019-06-21 21:30:14 +0000

    script corrections after first test

--- new.sh
+++ new.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 epitech_login="xavier.le-cunff@epitech.eu"
 personal_github="https://github.com/JohnBerd"
 
@@ -12,8 +14,8 @@ check_updates() {
     then
         apt-get upgrade
         apt-get -y dist-upgrade
-        echo ""
-        echo "\e[33mComputer will reboot in 10 seconds, please rerun the script\e[0m"
+        echo -e ""
+        echo -e "\e[33mComputer will reboot in 10 seconds, please rerun the script\e[0m"
         sleep 10
         reboot
     fi  
@@ -22,18 +24,18 @@ check_updates() {
 check_root() {
     if [ "$EUID" -ne 0 ]
     then
-        echo "Please run as root"
+        echo -e "Please run as root"
         exit
     fi
 }
 
 conclusion() {
     is_installed $1
-    if [ $inst != "" ]
+    if [ "$inst" != "" ]
     then
-        echo "\e[32m[+]\e[0m $1 is now installed"
+        echo -e "\e[32m[+]\e[0m $1 is now installed"
     else
-        echo "\e[31m[-]\e[0m Failed to install $1"
+        echo -e "\e[31m[-]\e[0m Failed to install $1"
     fi
 }
 
@@ -72,7 +74,7 @@ fi
 conclusion arduino
 
 is_installed mkspiffs
-if [ $inst = "" ]
+if [ "$inst" = "" ]
 then
     git clone https://github.com/igrr/mkspiffs.git
     cd mkspiffs
@@ -84,45 +86,45 @@ then
 fi
 conclusion mkspiffs
 
-is_installed google-chrome
-if [ $inst = "" ]
+is_installed atom
+if [ "$inst" = "" ]
 then
     wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-    sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+    sh -c 'echo -e "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
     sudo apt-get update
     apt-get install atom
 fi
-conclusion google-chrome
+conclusion atom
 
-is_installed atom
-if [ $inst = "" ]
+is_installed google-chrome-stable
+if [ "$inst" = "" ]
 then
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     apt install ./google-chrome-stable_current_amd64.deb
     rm google-chrome-stable_current_amd64.deb
 fi
-conclusion atom
+conclusion google-chrome-stable
 
 is_installed code
 # visual studio code
-if [ $inst = "" ]
+if [ "$inst" = "" ]
 then
-    echo "\e[33m[.]\e[0m Installing visual studio code"
+    echo -e "\e[33m[.]\e[0m Installing visual studio code"
     curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
     install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
     rm microsoft.gpg
-    sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sh -c 'echo -e "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
     sudo apt-get install apt-transport-https
     sudo apt-get update
-    apt-install code
+    apt install code
 fi
 conclusion code
 
 #shell
 if [ "`cat ~/.zshrc | grep agnoster`" = "" ]
 then
-    echo "\e[33m[.]\e[0m Installing zsh config"
+    echo -e "\e[33m[.]\e[0m Installing zsh config"
     chsh -s /bin/zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     sed -i 's/robbyrussell/agnoster/g' ~/.zshrc
@@ -131,16 +133,17 @@ then
     mv terminator_search.py ~/.config/terminator/plugins
     cp config ~/.config/terminator/
     mkdir -p ~/.zsh
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh
-    echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+    mv zsh-syntax-highlighting ~/.zsh
+    echo -e "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
     cat .zshbonus >> ~/.zshrc
 fi
 
 #android-studio
 is_installed studio
-if [ $inst = "" ]
+if [ "$inst" = "" ]
 then
-    echo "\e[33m[.]\e[0m Installing android studio"
+    echo -e "\e[33m[.]\e[0m Installing android studio"
     wget https://dl.google.com/dl/android/studio/ide-zips/3.4.1.0/android-studio-ide-183.5522156-linux.tar.gz
     tar -xzvf android-studio-ide-183.5522156-linux.tar.gz
     rm android-studio-ide-183.5522156-linux.tar.gz
@@ -150,9 +153,9 @@ fi
 conclusion studio
 
 #vim
-if [ ! -d ~/.vim/bundle ]
+if [ ! -f ~/.vimrc ]
 then
-    echo -e "\e[33m[.]\e[0m Installing vim config"
+    echo -e -e "\e[33m[.]\e[0m Installing vim config"
     git submodule update --init --recursive
     cp .vimrc ~
     cp -r .vim ~
@@ -161,15 +164,16 @@ fi
 
 #blih
 is_installed blih
-if [ $inst = "" ]
+if [ "$inst" = "" ]
 then
-    echo "\e[33m[.]\e[31m Installing blih"
+    echo -e "\e[33m[.]\e[31m Installing blih"
     cp programs/blih /usr/local/bin
 fi
 conclusion blih
 
 #theme
-if [ "`ls /boot/grub/themes`" = "0" ]
+mkdir -p /boot/grub/themes
+if [ "`ls /boot/grub/themes | wc -l`" = "0" ]
 then
     mkdir -p /boot/grub/themes
     wget https://bitbucket.org/gemlion/aurora-penguinis/raw/master/Aurora-Penguinis-GRUB2.tar.gz
@@ -178,10 +182,10 @@ then
     mv Aurora-Penguinis-GRUB2 /boot/grub/themes
     rm Aurora-Penguinis-GRUB2.tar.gz
     sed -i '/GRUB_THEME/d' /etc/default/grub
-    echo "GRUB_THEME=\"/boot/grub/themes/Aurora-Penguinis-GRUB2/theme.txt\"" >> /etc/default/grub
+    echo -e "GRUB_THEME=\"/boot/grub/themes/Aurora-Penguinis-GRUB2/theme.txt\"" >> /etc/default/grub
     grub-mkconfig -o /boot/grub/grub.cfg
 
-    apt install libglib2.0-dev-bin imagemagick plymouth plymouth-themes plank
+    apt install -y libglib2.0-dev-bin imagemagick plymouth plymouth-themes plank
     git clone https://github.com/daniruiz/Flat-Remix
     git clone https://github.com/daniruiz/Flat-Remix-GNOME-theme
     git clone https://github.com/vinceliuice/vimix-gtk-themes.git
@@ -200,22 +204,22 @@ then
     cp -r boot-theme/slash /usr/share/plymouth/themes
     plymouth-set-default-theme -R slash
     sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/d' /etc/default/grub
-    echo "GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash\"" >> /etc/default/grub
+    echo -e "GRUB_CMDLINE_LINUX_DEFAULT=\"quiet splash\"" >> /etc/default/grub
     update-grub2
 
     mkdir -p ~/.config/autostart/
-    cp plank.destop ~/.config/autostart/
+    cp plank.desktop ~/.config/autostart/
 fi
 
 #git
 if [ ! -f ~/.ssh/id_rsa.pub ]
 then
-    echo "\e[33m[.]\e[0m Installing ssh config"
+    echo -e "\e[33m[.]\e[0m Installing ssh config"
     ssh-keygen
     xclip -sel clip < ~/.ssh/id_rsa.pub
     blih -u $epitech_login sshkey upload ~/.ssh/id_rsa.pub
     firefox $personnal_github&
-    echo "You can now paste your ssh key in github"
+    echo -e "You can now paste your ssh key in github"
 fi
 
 firefox https://adblockplus.org/fr/&
